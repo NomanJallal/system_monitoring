564809ea4e339f75a2e3ca2164b2aeea3bf5c874
Noman Jalal <nomanjalaal@gmail.com>
2024-04-16 21:58:44 +0100

    Add files via upload

---
+++ system_monitoring.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sock import Sock
+from time import sleep
+import psutil
+
+app = Flask(__name__)
+sock = Sock(app)
+
+@sock.route('/connect')
+def handle_connect(ws):
+    while True:
+        system_usage = {
+            'cpu_usage': psutil.cpu_percent(interval=1),
+            'memory_usage': psutil.virtual_memory().used / (1024 * 1024), # Memory usage in MB
+            'storage_usage': psutil.disk_usage('/').used / (1024 * 1024 * 1024)  # Storage usage in GB
+        }
+        ws.send(system_usage)
+        sleep(1)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080, debug=True)
